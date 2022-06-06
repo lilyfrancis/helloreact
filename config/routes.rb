8ac92d8d7b36a951e7d97982eb8668920a369222
lilyfrancis <ifnotlily@yahoo.com>
2022-06-06 14:34:46 +0100

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,4 @@
 Rails.application.routes.draw do
-  # get 'static/index'
-  # get 'pages/index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 
   namespace :api, default: {format: :json} do
     namespace :v1,  default: {format: :json} do
